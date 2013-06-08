1235f0389b54809581cf699fa4253079a737cc77
Peter Bex <peter@more-magic.net>
2013-06-08 15:10:30 +0000

    r7rs: Fix call-with-port, it should only close the port on normal return.  Add a test for this.

--- tests/run.scm
+++ tests/run.scm
@@ -15,45 +15,69 @@
     ((_ . body) (handle-exceptions e e . body))))
 
 (test-group "exceptions"
- (test "with-exception-handler (escape)"
-  'exception
-  (call-with-current-continuation
-   (lambda (k)
-    (with-exception-handler
-     (lambda (e) (k 'exception))
-     (lambda () (+ 1 (raise 'an-error)))))))
- (test-error "with-exception-handler (return)"
-  (with-exception-handler
-   (lambda (e) 'ignore)
-   (lambda () (+ 1 (raise 'an-error)))))
- (test-error "with-exception-handler (raise)"
-  (with-exception-handler
-   (lambda (e) 'ignore)
-   (lambda () (+ 1 (raise 'an-error)))))
- (test "with-exception-handler (raise-continuable)"
-  65
-  (with-exception-handler
-   (lambda (e) 42)
-   (lambda () (+ (raise-continuable "should be a number") 23))))
- (test "error-object? (#f)" #f (error-object? 'no))
- (test "error-object? (#t)" #t (error-object? (catch (car '()))))
- (test "error-object-message" "fubar" (error-object-message (catch (error "fubar"))))
- (test "error-object-irritants" '(42) (error-object-irritants (catch (error "fubar" 42))))
- (test "read-error? (#f)" #f (read-error? (catch (car '()))))
- (test "read-error? (#t)" #t (read-error? (catch (read-from-string ")"))))
- (test "file-error? (#f)" #f (file-error? (catch (car '()))))
- (test "file-error? (#t)" #t (file-error? (catch (open-input-file "foo"))))
- (test-error "guard (no match)"
-  (guard (condition ((assq 'c condition))) (raise '((a . 42)))))
- (test "guard (match)"
-  '(b . 23)
-  (guard (condition ((assq 'b condition))) (raise '((b . 23)))))
- (test "guard (=>)"
-  42
-  (guard (condition ((assq 'a condition) => cdr)) (raise '((a . 42)))))
- (test "guard (multiple)"
-  '(b . 23)
-  (guard (condition
-          ((assq 'a condition) => cdr)
-          ((assq 'b condition)))
-    (raise '((b . 23))))))
+  (test "with-exception-handler (escape)"
+        'exception
+        (call-with-current-continuation
+         (lambda (k)
+           (with-exception-handler
+            (lambda (e) (k 'exception))
+            (lambda () (+ 1 (raise 'an-error)))))))
+  (test-error "with-exception-handler (return)"
+              (with-exception-handler
+               (lambda (e) 'ignore)
+               (lambda () (+ 1 (raise 'an-error)))))
+  (test-error "with-exception-handler (raise)"
+              (with-exception-handler
+               (lambda (e) 'ignore)
+               (lambda () (+ 1 (raise 'an-error)))))
+  (test "with-exception-handler (raise-continuable)"
+        65
+        (with-exception-handler
+         (lambda (e) 42)
+         (lambda () (+ (raise-continuable "should be a number") 23))))
+  (test "error-object? (#f)" #f (error-object? 'no))
+  (test "error-object? (#t)" #t (error-object? (catch (car '()))))
+  (test "error-object-message" "fubar" (error-object-message (catch (error "fubar"))))
+  (test "error-object-irritants" '(42) (error-object-irritants (catch (error "fubar" 42))))
+  (test "read-error? (#f)" #f (read-error? (catch (car '()))))
+  (test "read-error? (#t)" #t (read-error? (catch (read-from-string ")"))))
+  (test "file-error? (#f)" #f (file-error? (catch (car '()))))
+  (test "file-error? (#t)" #t (file-error? (catch (open-input-file "foo"))))
+  (test-error "guard (no match)"
+              (guard (condition ((assq 'c condition))) (raise '((a . 42)))))
+  (test "guard (match)"
+        '(b . 23)
+        (guard (condition ((assq 'b condition))) (raise '((b . 23)))))
+  (test "guard (=>)"
+        42
+        (guard (condition ((assq 'a condition) => cdr)) (raise '((a . 42)))))
+  (test "guard (multiple)"
+        '(b . 23)
+        (guard (condition
+                ((assq 'a condition) => cdr)
+                ((assq 'b condition)))
+               (raise '((b . 23))))))
+
+;; call-with-port is not supposed to close its port when leaving the
+;; dynamic extent, only on normal return.
+(test-group "ports"
+  (let*-values (((jump-back? jump!) (call/cc (lambda (k) (values #f k))))
+                (_ (and jump-back? (jump! (void))))
+                ((string)
+                 (call-with-output-string
+                  (lambda (the-string-port)
+                    (test "call-with-port returns all values yielded by proc"
+                          '(1 2 3)
+                          (receive (call-with-port
+                                    the-string-port
+                                    (lambda (p)
+                                      (display "foo" p)
+                                      ;; Leave the dynamic extent momentarily;
+                                      ;; jump! will immediately return with #t.
+                                      (call/cc (lambda (k) (jump! #t k)))
+                                      (display "bar" p)
+                                      (values 1 2 3)))))
+                    (test-assert "call-with-port closes the port on normal return"
+                                 (port-closed? the-string-port))))))
+    (test "call-with-port passes the port correctly and allows temporary escapes"
+          "foobar" string)))
