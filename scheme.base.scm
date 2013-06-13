cf0a0a6ecbbf96da74b78598f78d44a2cac5e8f1
Peter Bex <peter@more-magic.net>
2013-06-13 19:14:25 +0000

    R7RS: Fix with-exception-handler and "guard"; improve tests a bit

--- scheme.base.scm
+++ scheme.base.scm
@@ -37,69 +37,70 @@
 ;;;
 
 ;; guard & guard-aux copied verbatim from the draft.
+;; guard-aux put in a letrec-syntax due to import/export issues...
 (define-syntax guard
   (syntax-rules ()
     ((guard (var clause ...) e1 e2 ...)
-     ((call/cc
+     (letrec-syntax ((guard-aux 
+                      (syntax-rules ___ (else =>)
+                        ((guard-aux reraise (else result1 result2 ___))
+                         (begin result1 result2 ___))
+                        ((guard-aux reraise (test => result))
+                         (let ((temp test))
+                           (if temp
+                               (result temp)
+                               reraise)))
+                        ((guard-aux reraise (test => result)
+                                    clause1 clause2 ___)
+                         (let ((temp test))
+                           (if temp
+                               (result temp)
+                               (guard-aux reraise clause1 clause2 ___))))
+                        ((guard-aux reraise (test))
+                         (or test reraise))
+                        ((guard-aux reraise (test) clause1 clause2 ___)
+                         (let ((temp test))
+                           (if temp
+                               temp
+                               (guard-aux reraise clause1 clause2 ___))))
+                        ((guard-aux reraise (test result1 result2 ___))
+                         (if test
+                             (begin result1 result2 ___)
+                             reraise))
+                        ((guard-aux reraise
+                                    (test result1 result2 ___)
+                                    clause1 clause2 ___)
+                         (if test
+                             (begin result1 result2 ___)
+                             (guard-aux reraise clause1 clause2 ___))))))
+      ((call/cc
         (lambda (guard-k)
           (with-exception-handler
            (lambda (condition)
              ((call/cc
-                (lambda (handler-k)
-                  (guard-k
-                   (lambda ()
-                     (let ((var condition))
-                       (guard-aux
-                        (handler-k
-                         (lambda ()
-                           (raise-continuable condition)))
-                        clause ...))))))))
+               (lambda (handler-k)
+                 (guard-k
+                  (lambda ()
+                    (let ((var condition))
+                      (guard-aux
+                       (handler-k
+                        (lambda ()
+                          (raise-continuable condition)))
+                       clause ...))))))))
            (lambda ()
              (call-with-values
-              (lambda () e1 e2 ...)
-              (lambda args
-                (guard-k
-                 (lambda ()
-                   (apply values args)))))))))))))
-
-(define-syntax guard-aux
-  (syntax-rules (else =>)
-    ((guard-aux reraise (else result1 result2 ...))
-     (begin result1 result2 ...))
-    ((guard-aux reraise (test => result))
-     (let ((temp test))
-       (if temp
-           (result temp)
-           reraise)))
-    ((guard-aux reraise (test => result)
-                clause1 clause2 ...)
-     (let ((temp test))
-       (if temp
-           (result temp)
-           (guard-aux reraise clause1 clause2 ...))))
-    ((guard-aux reraise (test))
-     (or test reraise))
-    ((guard-aux reraise (test) clause1 clause2 ...)
-     (let ((temp test))
-       (if temp
-           temp
-           (guard-aux reraise clause1 clause2 ...))))
-    ((guard-aux reraise (test result1 result2 ...))
-     (if test
-         (begin result1 result2 ...)
-         reraise))
-    ((guard-aux reraise
-                (test result1 result2 ...)
-                clause1 clause2 ...)
-     (if test
-         (begin result1 result2 ...)
-         (guard-aux reraise clause1 clause2 ...)))))
+                 (lambda () e1 e2 ...)
+               (lambda args
+                 (guard-k
+                  (lambda ()
+                    (apply values args))))))))))))))
 
 
 ;;;
 ;;; 6.11. Exceptions
 ;;;
 
+;; XXX TODO: This is not threadsafe!
 (define-values (with-exception-handler raise raise-continuable)
   (let ((exception-handlers
          (let ((lst (list ##sys#current-exception-handler)))
@@ -110,6 +111,11 @@
      (lambda (handler thunk)
        (dynamic-wind
         (lambda ()
+          ;; We might be interoperating with srfi-12 handlers set by intermediate
+          ;; non-R7RS code, so check if a new handler was set in the meanwhile.
+          (unless (eq? (car exception-handlers) ##sys#current-exception-handler)
+            (set! exception-handlers
+              (cons ##sys#current-exception-handler exception-handlers)))
           (set! exception-handlers (cons handler exception-handlers))
           (set! ##sys#current-exception-handler handler))
         thunk
