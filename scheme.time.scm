d2b8ed302e3cf452fa92c366a8562ff867ca8d06
Evan Hanson <evhan@foldling.org>
2014-06-17 06:04:50 +0000

    Include TAI offset in current-second

--- scheme.time.scm
+++ scheme.time.scm
@@ -2,8 +2,13 @@
 		     current-jiffy
 		     jiffies-per-second)
   (import (only scheme define inexact->exact)
-	  (rename (only chicken current-seconds current-milliseconds :)
-		  (current-seconds current-second)))
+	  (only chicken : define-constant current-seconds current-milliseconds fp+))
+
+  ;; As of 2012-06-30.
+  (define-constant tai-offset 35.)
+
+  (: current-second (--> float))
+  (define (current-second) (fp+ (current-seconds) tai-offset))
 
   (: current-jiffy (--> fixnum))
   (define (current-jiffy) (inexact->exact (current-milliseconds)))
