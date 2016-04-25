091911d32921985c940ed24814049a8b7840617e
Evan Hanson <evhan@foldling.org>
2016-04-26 10:09:08 +1200

    Fix expansion of `include-library-declarations`
    Previously, it would incorrectly splice a "##core#begin" into the
    expansion.
    
    Also, drop the unnecessary "##core#begin" from the "##core#module" forms
    that result from `define-library` expansions.

--- r7rs-compile-time.scm
+++ r7rs-compile-time.scm
@@ -146,7 +146,7 @@
 	      ,(parse-decls more)))
 	   ((('include-library-declarations fnames ...) . more)
 	    `(##core#begin
-	      ,@(process-include-decls fnames)
+	      ,(process-include-decls fnames)
 	      ,(parse-decls more)))
 	   ((('cond-expand decls ...) . more)
 	    `(##core#begin
@@ -157,17 +157,16 @@
 	      ,@code
 	      ,(parse-decls more)))
 	   (decl (syntax-error 'define-library "invalid library declaration" decl))))
-       `(##core#begin
-	 (##core#module
-	  ,real-name ((,dummy-export))
-	  ;; gruesome hack: we add a dummy export for adding indirect exports
-	  (##core#define-syntax ,dummy-export (##core#lambda _ '(##core#undefined)))
-	  ;; Another gruesome hack: provide feature so "use" works properly
-	  (##sys#provide (##core#quote ,real-name))
-	  ;; Set up an R7RS environment for the module's body.
-	  (import-for-syntax r7rs) ; overwrites "syntax-rules"
-	  (import r7rs) ; overwrites existing "import" and "import-for-syntax"
-          ,(parse-decls decls)))))
+       `(##core#module
+         ,real-name ((,dummy-export))
+         ;; gruesome hack: we add a dummy export for adding indirect exports
+         (##core#define-syntax ,dummy-export (##core#lambda _ '(##core#undefined)))
+         ;; Another gruesome hack: provide feature so "use" works properly
+         (##sys#provide (##core#quote ,real-name))
+         ;; Set up an R7RS environment for the module's body.
+         (import-for-syntax r7rs) ; overwrites "syntax-rules"
+         (import r7rs) ; overwrites existing "import" and "import-for-syntax"
+         ,(parse-decls decls))))
     (_ (syntax-error 'define-library "invalid library definition" form))))
 
 (define (register-r7rs-module name)
