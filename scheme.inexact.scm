fb4f0d933e8204fd31c5317bd0ddbc4370c2dbcd
Evan Hanson <evhan@foldling.org>
2016-09-20 11:32:34 +1200

    Drop export of nonexistent "infinite?" from scheme.inexact with no-numbers

--- scheme.inexact.scm
+++ scheme.inexact.scm
@@ -3,7 +3,7 @@
   (cond-expand
     (no-numbers
      (import chicken)
-     (export acos asin atan exp infinite? sin cos tan finite? log sqrt))
+     (export acos asin atan exp sin cos tan finite? log sqrt))
     (else
      (import numbers)
      (export acos asin atan exp infinite? sin cos tan finite? log sqrt nan?))))
